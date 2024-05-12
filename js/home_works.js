bd158bcbdda6137cf26ca48d9bb1e37f85d6bdcc
almanbetovaa <a83382576@gmail.com>
2024-05-12 12:22:17 +0600

    dz1

--- js/home_works.js
+++ js/home_works.js
@@ -19,11 +19,36 @@ const child = document.querySelector('.child_block')
 let positionX = 0
 let positionY = 0
 
+const maxWidth = parent.offsetWidth - child.offsetWidth
+const maxHeight = parent.offsetHeight - child.offsetHeight
+
 const moveBlock = () => {
-    if (positionX < 449){
+    if (positionX < maxWidth && positionY === 0) {
         positionX++
         child.style.left = `${positionX}px`
+        requestAnimationFrame(moveBlock)
+    } else if (positionX >= maxWidth && positionY < maxHeight) {
+        positionY++
+        child.style.top = `${positionY}px`
+        requestAnimationFrame(moveBlock)
+    } else if (positionY >= maxHeight && positionX > 0) {
+        positionX--
+        child.style.left = `${positionX}px`
+        requestAnimationFrame(moveBlock)
+    } else if (positionY > 0 && positionX === 0) {
+        positionY--
+        child.style.top = `${positionY}px`
         requestAnimationFrame(moveBlock)
     }
 }
 moveBlock()
+
+
+
+
+
+
+
+
+
+
