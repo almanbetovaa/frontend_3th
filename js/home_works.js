dc7add94f55c6ea759017af84f8bf4000a852479
almanbetovaa <a83382576@gmail.com>
2024-05-12 13:11:29 +0600

    dz2

--- js/home_works.js
+++ js/home_works.js
@@ -44,6 +44,37 @@ const moveBlock = () => {
 moveBlock()
 
 
+const start = document.querySelector('#start')
+const stop = document.querySelector('#stop')
+const reset = document.querySelector('#reset')
+
+let timer = 0
+let interval = null
+
+function counter (){
+    document.getElementById("seconds").textContent= timer
+}
+function startCounter (){
+    clearInterval(interval)
+    interval= setInterval(()=> {
+        timer++
+        counter()
+    }, 1000)
+}
+
+function stopCounter (){
+    clearInterval(interval)
+}
+function resetCounter (){
+    clearInterval(interval)
+    timer = 0
+    counter()
+}
+start.addEventListener("click", startCounter);
+stop.addEventListener("click", stopCounter);
+reset.addEventListener("click", resetCounter);
+
+
 
 
 
