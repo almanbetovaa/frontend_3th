686104b53850454ee5717c5c8c1a1eab78bee7eb
almanbetovaa <a83382576@gmail.com>
2024-05-14 21:03:48 +0600

    dz3

--- js/lesson.js
+++ js/lesson.js
@@ -13,3 +13,52 @@ phoneButton.onclick = () => {
         phoneResult.style.color = 'red'
     }
 }
+
+// Tab slider
+const tabContentBlocks = document.querySelectorAll('.tab_content_block')
+
+const tabContentItems = document.querySelectorAll('.tab_content_item')
+const tabParent = document.querySelector('.tab_content_items')
+let currentIndex = 0;
+let intervalId
+
+const hideTabContent = () => {
+    tabContentBlocks.forEach( (item) => {
+        item.style.display = 'none'
+    })
+    tabContentItems.forEach((item) => {
+        item.classList.remove('tab_content_item_active')
+    } )
+}
+const showTabContent = (index = 0) => {
+    tabContentBlocks[index].style.display = 'block'
+    tabContentItems[index].classList.add('tab_content_item_active')
+}
+const startSlider = () => {
+    intervalId = setInterval(() => {
+        hideTabContent()
+        currentIndex = (currentIndex + 1) % tabContentItems.length
+        showTabContent(currentIndex)
+    }, 3000)
+};
+
+const stopSlider = () => {
+    clearInterval(intervalId);
+}
+hideTabContent()
+showTabContent()
+startSlider()
+
+tabParent.onclick = (event)=> {
+    if (event.target.classList.contains('tab_content_item')){
+        tabContentItems.forEach((item, index) => {
+            if (event.target === item){
+                currentIndex = index
+                hideTabContent()
+                showTabContent(index)
+                stopSlider()
+                startSlider()
+            }
+        })
+    }
+}
---
+++ js/modal.js
@@ -0,0 +1,45 @@
+// modal
+
+const modal = document.querySelector('.modal')
+const modalTrigger = document.querySelector('#btn-get')
+const modalCloseButton = document.querySelector('.modal_close')
+let ModalShown = false
+
+const openModal = () => {
+    modal.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+}
+
+
+const closeModal = () => {
+    modal.style.display = 'none'
+    document.body.style.overflow = ''
+}
+
+modalTrigger.onclick = () => {
+    openModal()
+}
+ modalCloseButton.onclick = () =>{
+    closeModal()
+ }
+ modal.onclick = (event) => {
+    if (event.target === modal) {
+        closeModal()
+    }
+ }
+const handleScroll =() => {
+    const scrolledToBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight
+    if (scrolledToBottom && ModalShown) {
+        openModal()
+        window.removeEventListener('scroll', handleScroll)
+    }
+}
+
+window.addEventListener('scroll', handleScroll)
+
+setTimeout(() =>{
+    if (ModalShown) {
+        openModal()
+        window.removeEventListener('scroll', handleScroll)
+    }
+}, 10000)
