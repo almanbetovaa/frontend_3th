de0b3131321d6f9e0c32d9b1dc9631cb20a2c0c6
almanbetovaa <a83382576@gmail.com>
2024-05-28 16:38:44 +0600

    dz7-2

--- js/card.js
+++ js/card.js
@@ -1,22 +1,20 @@
-window.onload = async() =>{
+const cardContainer= document.getElementById('cardContainer')
+const Cards = async () => {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts')
         const data = await response.json()
-        renderCards(data)
-    } catch (error) {
-        console.log(error)
-    }
-}
-const renderCards =(data) => {
-    const cardContainer= document.getElementById('cardContainer')
-    data.forEach(cardData => {
-        const card = document.createElement('div')
-        card.classList.add('card')
-        card.innerHTML = `
+        data.forEach(cardData => {
+            const card = document.createElement('div')
+            card.classList.add('card')
+            card.innerHTML = `
                     <img src="https://itproger.com/img/courses/1476977754.jpg" alt="JS image">
                     <h2>${cardData.title}</h2>
                     <p>${cardData.body}</p>
                 `
-        cardContainer.appendChild(card)
-    })
+            cardContainer.appendChild(card)
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
+window.onload = Cards;
